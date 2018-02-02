30eb2583526727a4d903e5a8c067117cfc5dcc82
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 20:12:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function Objects () {
+  
+}
+
+function updateObjectWithKeyAndValue (object, key, value) {
+  return Object.assign ({}, object, {[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyandValue () {
+  
+}
